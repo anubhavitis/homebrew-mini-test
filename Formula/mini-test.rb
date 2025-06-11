4ce31f9c0e04b33796f3cfc8f6408fea8d722fe9
Anubhav Singhal <ss.lfsgd@gmail.com>
2025-06-11 10:34:59 +0530

    added test in formula

--- Formula/mini-test.rb
+++ Formula/mini-test.rb
@@ -49,15 +49,7 @@ class MiniTest < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test mini-test`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
-  end
+  system bin/"ptree", "--help" # Basic command check
+  end 
+  
 end
